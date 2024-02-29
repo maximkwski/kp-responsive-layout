fdd3028ded62110f5dfa14db3266b334b112d4f4
Maksym <makkie.makowski@gmail.com>
2024-02-29 16:31:28 -0500

    fix nav bar styles

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ const nav = document.querySelector('.nav');
 
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('nav--visible');
+    if (nav.classList.contains('nav--visible')) {
+        nav.style.maxHeight = nav.scrollHeight + "px";
+    } else {
+        nav.style.maxHeight = 0;
+    }
 })
